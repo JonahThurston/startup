3672754097e014e768da4686fd21cb358fb56c2c
Jonah Thurston <jonah.m.thurston@gmail.com>
2024-02-27 12:53:21 -0700

    Display UserName

---
+++ watchList.js
@@ -0,0 +1,13 @@
+class WatchList {
+
+    constructor() {
+        const playerNameEl = document.querySelector('.player-name');
+        playerNameEl.textContent = this.getPlayerName();
+    }
+
+    getPlayerName() {
+        return localStorage.getItem('userName') ?? 'Mystery player';
+    }
+}
+
+const watchList = new WatchList();
