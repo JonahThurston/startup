6605c059895da7c00b1ee59f172150ad97de90a5
Jonah Thurston <jonah.m.thurston@gmail.com>
2024-02-27 13:12:38 -0700

    simulate web socket

--- watchList.js
+++ watchList.js
@@ -11,3 +11,11 @@ class WatchList {
 }
 
 const watchList = new WatchList();
+
+// Simulate chat messages that will come over WebSocket
+setInterval(() => {
+    const notificationText = document.querySelector('#notificationList');
+    notificationText.innerHTML =
+      `<li class="notification">Marcus just watched Lego Batman</li>` +
+      notificationText.innerHTML;
+}, 5000);
