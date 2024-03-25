8c09cd8c2b839a402b6d3c21026d137cf1ed136e
Jonah Thurston <jonah.m.thurston@gmail.com>
2024-03-25 00:54:26 -0600

    database functions

--- index.js
+++ index.js
@@ -1,40 +1,118 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
-const serverStorage = new Map();
+const DB = require('./database.js');
+
+const authCookieName = 'token';
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
+
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
 
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await DB.getUser(req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUser(req.body.email, req.body.password);
+
+    // Set the cookie
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
+  }
+});
+
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// GetUser returns information about a user
+apiRouter.get('/user/:email', async (req, res) => {
+  const user = await DB.getUser(req.params.email);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ email: user.email, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
+
+// secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
 // Get score
-apiRouter.get('/getScore/:name', (req, res) => {
-  let scoresToGet = `${req.params.name}Score`
-  if(serverStorage.has(scoresToGet)){
-    scoreObject = serverStorage.get(scoresToGet)
+secureApiRouter.get('/getScore/:name', async (req, res) => {
+  const scoreObject = await DB.getScore(req.params.name);
+  if(scoreObject){
     res.send(scoreObject);
-  } else{
+  }
+  else{
     scoreObject = {score: 0};
-    serverStorage.set(scoresToGet, scoreObject)
+    //TODO: serverStorage.set(scoresToGet, scoreObject)
     res.send(scoreObject);
   }
 });
 
+// set a score for username
+secureApiRouter.post('/setScore/:name', async (req, res, next) => {
+  let name = req.params.name
+  const score = { ...req.body, name: name };
+  await DB.addScore(score, name);
+  res.send(score);
+});
+
 // Get table
-apiRouter.get('/getTable/:name', (req, res) => {
-  let tableToGet = `${req.params.name}Table`
-  if(serverStorage.has(tableToGet)){
-    tableObject = serverStorage.get(tableToGet)
-    res.send(tableObject);
-  } else{
+secureApiRouter.get('/getTable/:name', async (req, res) => {
+  const tableObject = await DB.getTable(req.params.name);
+  if(tableObject){
+    res.send(tableObject); 
+  }
+  else{
     tableObject = [
       false, false, false, false , false, false, false, false, false, false,
       false, false, false, false , false, false, false, false, false, false,
@@ -42,31 +120,38 @@ apiRouter.get('/getTable/:name', (req, res) => {
       false, false, false, false , false, false, false, false, false, false,
       false, false, false, false , false, false, false, false, false, false,
       ];
-    serverStorage.set(tableToGet, tableObject)
+    //TODO: serverStorage.set(tableToGet, tableObject)
     res.send(tableObject);
   }
 });
 
 // set a table for username
-apiRouter.post('/setTable/:name', (req, res, next) => {
+secureApiRouter.post('/setTable/:name', async (req, res, next) => {
+  let name = req.params.name
+  const table = { ...req.body, name: name };
+  await DB.addTable(table, name);
+  res.send(score);
+  /*
     let name = req.params.name
     let tableToSet = `${name}Table`
     serverStorage.set(tableToSet, req.body)
     res.send(serverStorage.get(tableToSet));
-});
-
-// set a score for username
-apiRouter.post('/setScore/:name', (req, res, next) => {
-  let name = req.params.name
-  let scoreToSet = `${name}Score`
-  serverStorage.set(scoreToSet, req.body)
-  res.send(serverStorage.get(scoreToSet));
+    */
 });
 
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
