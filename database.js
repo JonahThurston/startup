bd6d0777d395fa81c3ddfc7d046eb466f389cbc0
Jonah Thurston <jonah.m.thurston@gmail.com>
2024-03-25 16:06:48 -0600

    put to correct collection

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
+const db = client.db('startup');
 const userCollection  = db.collection('user');
 const scoreCollection = db.collection('score');
 const tableCollection = db.collection('table');
