6bb06e8ef1219514b26c396b2beede0fb4bab9d2
Jonah Thurston <jonah.m.thurston@gmail.com>
2024-04-12 14:37:06 -0600

    make jsx stubs

---
+++ src/watchlist/watchlist.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function watchList() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>watchList displayed here</div>
+    </main>
+  );
+}
